fb52f7c7904a703759febb10caa5c2a1ade65304
Veronika Emelyanchenko <v.emelyanchenko@gmail.com>
2026-02-09 15:44:25 +0300

    add logic

--- js/script.js
+++ js/script.js
@@ -1,10 +1,8 @@
 console.log('Скрипт работает!');
 
-document.body.addEventListener('click', function() {
-    document.body.style.backgroundColor = 
-        document.body.style.backgroundColor === 'lightblue' ? '#f0f0f0' : 'lightblue';
-});
-
-document.querySelector('h1').addEventListener('mouseenter', function() {
-    console.log('Вы навели курсор на заголовок!');
+document.body.addEventListener('click', function () {
+  document.body.style.backgroundColor =
+    document.body.style.backgroundColor === 'lightblue'
+      ? '#f0f0f0'
+      : 'lightblue';
 });
